b2a884182fd54f71a7f02fe5947a65d95f217d2b
Armando Rios <armandorios.dev@gmail.com>
2025-03-12 21:02:40 -0700

    feat: create getWeather function

---
+++ src/api/weatherService.js
@@ -0,0 +1,15 @@
+import env from "./config.js"
+
+const API_KEY = env.WEATHER_API
+
+export const getWeather = async (place) => {
+  try {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=metric&key=${API_KEY}`)
+    const data = await response.json()
+    return data
+  }
+  catch (error) {
+    console.log(error)
+    return error
+  }
+}
