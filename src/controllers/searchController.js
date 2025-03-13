b4c518d593e01815156e7c37e7d9a8f8f587e66c
Armando Rios <armandorios.dev@gmail.com>
2025-03-13 02:20:46 -0700

    feat: create search controller

---
+++ src/controllers/searchController.js
@@ -0,0 +1,12 @@
+import { renderWeather } from "../views/renderWeather.js"
+
+export const initSearchController = () => {
+  const searchForm = document.getElementById("search-form")
+  const searchInput = document.getElementById("search-input")
+
+  searchForm.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    renderWeather(searchInput.value)
+    searchInput.value = ""
+  })
+}
