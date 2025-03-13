e5fbdf59ddb9d61a5900f000f7168ab5fe4a0192
Armando Rios <armandorios.dev@gmail.com>
2025-03-13 02:56:45 -0700

    feat: use short name

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,10 +1,12 @@
 export const saveLocation = (location) => {
+  const locationArray = location.split(",")
   const savedLocations = JSON.parse(localStorage.getItem("savedLocations")) || []
-  if (savedLocations.includes(location)) {
+  if (savedLocations.includes(locationArray[0])) {
     return savedLocations
   }
-  savedLocations.push(location)
+  savedLocations.push(locationArray[0])
   localStorage.setItem("savedLocations", JSON.stringify(savedLocations))
+  return savedLocations
 }
 
 export const getSavedLocations = () => {
