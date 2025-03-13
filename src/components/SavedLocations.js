d08f438782e12633ec1a1e63acb46af08e16bd9e
Armando Rios <armandorios.dev@gmail.com>
2025-03-13 03:12:56 -0700

    feat: render saved Locations and temp

--- src/components/SavedLocations.js
+++ src/components/SavedLocations.js
@@ -1,9 +1,18 @@
+import { getWeather } from "../api/weatherService.js"
 const savedLocationsContainer = document.getElementById("saved-locations")
 export const SavedLocations = (savedLocations) => {
   savedLocationsContainer.innerHTML = ""
-  savedLocations.forEach((location) => {
+  savedLocations.forEach(async (location) => {
+    const data = await getWeather(location)
+    console.log(data)
     const locationButton = document.createElement("button")
-    locationButton.innerText = location
+    locationButton.className = "p-2 w-full flex justify-between"
+
+    const locationName = document.createElement("p")
+    locationName.innerText = location.slice(0, 20)
+    const locationTemp = document.createElement("p")
+    locationTemp.innerText = data.currentConditions.temp
+    locationButton.append(...[locationName, locationTemp])
     savedLocationsContainer.appendChild(locationButton)
   })
 }
