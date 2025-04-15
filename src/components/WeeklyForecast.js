ffd7df6f8fa17c6816d2179e4db8a4c836668c1b
Armando Rios <armandorios.dev@gmail.com>
2025-04-15 14:54:10 -0700

    styles: use grid for days forecast

--- src/components/WeeklyForecast.js
+++ src/components/WeeklyForecast.js
@@ -6,21 +6,27 @@ export const WeeklyForecast = days => {
     'flex flex-col h-80 overflow-auto items-center bg-white p-4 items-start border border-gray-200 rounded-xl';
 
   days.forEach(day => {
+    console.log(day);
     const { datetime, icon, temp, conditions } = day;
 
     const dayContainer = document.createElement('div');
-    dayContainer.className = 'flex items-center justify-center p-4';
+    dayContainer.className = 'grid grid-cols-6 p-4 gap-2 w-full';
 
     const date = document.createElement('p');
     date.className = 'text-sm text-gray-500';
     date.innerText = datetime;
 
     const weatherContainer = document.createElement('div');
-    weatherContainer.className = 'flex items-center';
+    weatherContainer.className = 'flex gap-2';
 
     const iconCondition = icon;
     const weatherIcon = WeatherIcon(iconCondition);
-    weatherContainer.appendChild(weatherIcon);
+
+    const condition = document.createElement('p');
+    condition.className = 'text-sm text-gray-500';
+    condition.innerText = conditions;
+
+    weatherContainer.append(...[weatherIcon, condition]);
 
     const tempSpan = document.createElement('span');
     tempSpan.className = 'ml-2 font-semibold';
@@ -28,6 +34,7 @@ export const WeeklyForecast = days => {
 
     dayContainer.appendChild(date);
     dayContainer.appendChild(weatherContainer);
+    // dayContainer.appendChild(condition);
     dayContainer.appendChild(tempSpan);
 
     weaklyContainer.appendChild(dayContainer);
