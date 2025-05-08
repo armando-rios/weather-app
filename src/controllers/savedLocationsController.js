58977da080118e69af5c7771aa5e6ed4a71e6c74
Armando Rios <armandorios.dev@gmail.com>
2025-05-08 10:41:18 -0700

    feat: add render forecast if exist save data

--- src/controllers/savedLocationsController.js
+++ src/controllers/savedLocationsController.js
@@ -1,7 +1,9 @@
 import { getSavedLocations } from '../utils/storage.js';
 import { SavedLocations } from '../components/SavedLocations.js';
+import { renderWeather } from '../views/renderWeather.js';
 
 export const initSavedLocationsController = () => {
   const savedLocations = getSavedLocations();
   SavedLocations(savedLocations, 'init');
+  renderWeather(savedLocations[0]);
 };
