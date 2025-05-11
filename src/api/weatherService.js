d87330197f06a82f56c0ef9d0830f8050ee8fa13
Armando Rios <armandorios.dev@gmail.com>
2025-05-11 16:41:55 -0700

    feat: implement date-based search caching to prevent duplicate API requests

--- src/api/weatherService.js
+++ src/api/weatherService.js
@@ -1,11 +1,22 @@
 const API_KEY = process.env.WEATHER_API;
 
+const searchCache = new Map();
+
 export const getWeather = async place => {
+  if (!place.trim()) return [];
+  if (searchCache.has(place)) {
+    console.log('Using cache for: ', place);
+    return searchCache.get(place);
+  }
+
+  console.log('Getting request for:', place);
+
   try {
     const response = await fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=metric&key=${API_KEY}`
     );
     const data = await response.json();
+    searchCache.set(place, data);
     return data;
   } catch (error) {
     return error;
--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,7 +1,8 @@
-import { getWeather } from '../api/weatherService.js';
+import { apiPromise, getWeather } from '../api/weatherService.js';
 import { saveLocation } from '../utils/storage.js';
 import { SavedLocations } from '../components/SavedLocations.js';
 import { renderWeather } from '../views/renderWeather.js';
+import { apiPromise, apiData } from '../api/weatherService.js';
 
 export const initSearchController = () => {
   const searchForm = document.getElementById('search-form');
@@ -9,13 +10,15 @@ export const initSearchController = () => {
 
   searchForm.addEventListener('submit', async e => {
     e.preventDefault();
+    apiPromise = null;
+    apiData = null;
 
     const value = await getWeather(searchInput.value);
 
-    const { resolvedAddress, currentConditions } = value;
+    const { resolvedAddress } = value;
 
     const savedLocations = saveLocation(resolvedAddress);
-    renderWeather(searchInput.value);
+    renderWeather(value);
     SavedLocations(savedLocations);
     searchInput.value = '';
   });
